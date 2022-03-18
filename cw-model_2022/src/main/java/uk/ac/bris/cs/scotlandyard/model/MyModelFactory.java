23d75402b9e60b7f0edb638582bbbfc07945e90c
Jadesola Bejide <vm21106@bristol.ac.uk>
2022-03-18 11:38:21 +0000

    Constructing the observer pattern

--- cw-model_2022/src/main/java/uk/ac/bris/cs/scotlandyard/model/MyModelFactory.java
+++ cw-model_2022/src/main/java/uk/ac/bris/cs/scotlandyard/model/MyModelFactory.java
@@ -4,7 +4,11 @@ import com.google.common.collect.ImmutableList;
 
 import javax.annotation.Nonnull;
 
+import com.google.common.collect.ImmutableSet;
 import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Factory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * cw-model
@@ -17,6 +21,41 @@ public final class MyModelFactory implements Factory<Model> {
 	                                      Player mrX,
 	                                      ImmutableList<Player> detectives) {
 		// TODO
-		throw new RuntimeException("Implement me!");
+		return new Model() {
+
+			private List<Observer> characters = new ArrayList<>();
+			private Board.GameState gs;
+
+			@Nonnull
+			@Override
+			public Board getCurrentBoard() {
+				return null;
+			}
+
+			@Override
+			public void registerObserver(@Nonnull Observer observer) {
+				if (observer == null) throw new NullPointerException("No null observers!");
+				if (characters.contains(observer)) throw new IllegalArgumentException("Can't have two of the same observer!");
+				characters.add(observer);
+			}
+
+			@Override
+			public void unregisterObserver(@Nonnull Observer observer) {
+				if (observer == null) throw new NullPointerException("No null observers!");
+				if (!characters.contains(observer)) throw new IllegalArgumentException("Observer not present!");
+				characters.remove(observer);
+			}
+
+			@Nonnull
+			@Override
+			public ImmutableSet<Observer> getObservers() {
+				return ImmutableSet.copyOf(characters);
+			}
+
+			@Override
+			public void chooseMove(@Nonnull Move move) {
+				System.out.println("");
+			}
+		};
 	}
 }
