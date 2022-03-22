55fad9689b891ba906090f90aa80f3ed175d7452
Jadesola Bejide <vm21106@bristol.ac.uk>
2022-03-22 16:48:30 +0000

    Adjusting dijkstra

--- cw-ai_2022/src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MyAi.java
+++ cw-ai_2022/src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MyAi.java
@@ -8,8 +8,9 @@ import java.util.function.Predicate;
 import javax.annotation.Nonnull;
 
 
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 import com.google.common.graph.ImmutableValueGraph;
 import io.atlassian.fugue.Pair;
 import uk.ac.bris.cs.scotlandyard.model.*;
@@ -19,7 +20,7 @@ public class MyAi implements Ai {
 	@Nonnull @Override public String name() { return "Name me!"; }
 
 	static Dictionary<Integer, ArrayList<Integer>> populate(Dictionary<Integer, ArrayList<Integer>> nodeDict,
-															ImmutableValueGraph<Integer, Integer> graph, Integer source){
+															ImmutableValueGraph<Integer, ImmutableSet<ScotlandYard.Transport>> graph, Integer source){
 		nodeDict.put(source, new ArrayList<Integer>(Arrays.asList(0, null)));
 		for(Object n : graph.nodes()){
 			if((Integer) n != source) {
@@ -33,8 +34,8 @@ public class MyAi implements Ai {
 		return Arrays.asList(nodes).contains(x);
 	}
 
-	static List<Integer> shortestPathFromSourceToDestination(
-			ImmutableValueGraph<Integer, Integer> graph,
+	static Pair<Integer, List<Integer>> shortestPathFromSourceToDestination(
+			ImmutableValueGraph<Integer, ImmutableSet<ScotlandYard.Transport>> graph,
 			Integer source,
 			Integer destination) {
 		Dictionary<Integer, ArrayList<Integer>> nodeDict = new Hashtable<Integer, ArrayList<Integer>>();
@@ -48,14 +49,16 @@ public class MyAi implements Ai {
 		Integer bestNode = -1;
 		ArrayList<Integer> endPoints = new ArrayList<>(); //places we know at least one distance to (we've calculated at least once)
 		endPoints.add(source);
+
+		//Track tickets
 		while (currentNode != destination) {
 
 			//getting successors
 			Object[] succ = graph.successors(currentNode).toArray();
 			for (Object node : succ) {
 				if(graph.edgeValue((Integer) node, currentNode).isPresent() && !MyAi.searchList(visitedNodes, (Integer)node) && (Integer)node != source) {
-					Integer edgeVal = graph.edgeValue((Integer) node, currentNode).get();
-					Integer distance = (nodeDict.get(currentNode).get(0) + edgeVal);
+					//each edge value is worth one (working version)
+					Integer distance = (nodeDict.get(currentNode).get(0) + 1);
 
 					//only update the distance if its shorter than our shortest to that node
 					if(distance < nodeDict.get(node).get(0)) { nodeDict.put((Integer) node, new ArrayList<Integer>(Arrays.asList(distance, currentNode))); }
@@ -84,13 +87,15 @@ public class MyAi implements Ai {
 
 		List<Integer> path = new ArrayList<Integer>();
 
+
 		path.add(0, currentNode);
 		while (true) {
 			currentNode = nodeDict.get(currentNode).get(1);
 			path.add(0, currentNode);
 			if(Objects.equals(currentNode, source)) {
 				System.out.println("Path: " +path);
-				return path;
+				Pair<Integer, List<Integer>> pathDistance = new Pair<Integer, List<Integer>>(nodeDict.get(destination).get(0), path);
+				return pathDistance;
 			} //needs to include source
 		}
 	}
@@ -105,11 +110,34 @@ public class MyAi implements Ai {
 		return null;
 	}
 
+	private Integer cumulativeDistance(Board board, List<Player> detectives, Player mrX) {
+		int min = Integer.MAX_VALUE;
+		Integer mrXLocation = mrX.location();
+		List<Integer> distancePerDetective = new ArrayList<>();
+		for (Player detective : detectives) {
+			Integer detectiveLocation = detective.location();
+			var path = shortestPathFromSourceToDestination(board.getSetup().graph, detectiveLocation, mrXLocation);
+			Integer distance = path.left();
+			List<Integer> nodes = path.right(); //may want to use for whatever reason
+			distancePerDetective.add(distance);
+		}
+
+		return distancePerDetective.stream().reduce(0, (x,y) -> x+y);
+	}
+
+	private Integer getDesiredDestination(Player mrX) {
+		int newLocation = mrX.location();
+
+
+	}
+
 	private Integer score() {
 		//score elements:
 		//distance from detectives (tickets away)
 		//available moves
 		//then call minimax
+
+		int distance = cumulativeDistance(null, null);
 
 		return null;
 	}
