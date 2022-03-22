1bdfef86203ab2ccb365cace80113b6c8d80461b
Jadesola Bejide <vm21106@bristol.ac.uk>
2022-03-22 15:41:09 +0000

    Hashing out the start

--- cw-ai_2022/src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MyAi.java
+++ cw-ai_2022/src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MyAi.java
@@ -1,20 +1,122 @@
 package uk.ac.bris.cs.scotlandyard.ui.ai;
 
-import java.util.Random;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.Predicate;
 
 import javax.annotation.Nonnull;
 
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.graph.ImmutableValueGraph;
 import io.atlassian.fugue.Pair;
-import uk.ac.bris.cs.scotlandyard.model.Ai;
-import uk.ac.bris.cs.scotlandyard.model.Board;
-import uk.ac.bris.cs.scotlandyard.model.Move;
+import uk.ac.bris.cs.scotlandyard.model.*;
 
 public class MyAi implements Ai {
 
 	@Nonnull @Override public String name() { return "Name me!"; }
+
+	static Dictionary<Integer, ArrayList<Integer>> populate(Dictionary<Integer, ArrayList<Integer>> nodeDict,
+															ImmutableValueGraph<Integer, Integer> graph, Integer source){
+		nodeDict.put(source, new ArrayList<Integer>(Arrays.asList(0, null)));
+		for(Object n : graph.nodes()){
+			if((Integer) n != source) {
+				nodeDict.put((Integer) n, new ArrayList<Integer>(Arrays.asList(Integer.MAX_VALUE, null)));
+			}
+		}
+		return nodeDict;
+	}
+
+	static boolean searchList(Integer[] nodes, Integer x) {
+		return Arrays.asList(nodes).contains(x);
+	}
+
+	static List<Integer> shortestPathFromSourceToDestination(
+			ImmutableValueGraph<Integer, Integer> graph,
+			Integer source,
+			Integer destination) {
+		Dictionary<Integer, ArrayList<Integer>> nodeDict = new Hashtable<Integer, ArrayList<Integer>>();
+
+		nodeDict = populate(nodeDict, graph, source);
+
+		int pos = 0;
+		Integer[] visitedNodes = new Integer[35];
+		Integer currentNode = source;
+		Integer min = Integer.MAX_VALUE;
+		Integer bestNode = -1;
+		ArrayList<Integer> endPoints = new ArrayList<>(); //places we know at least one distance to (we've calculated at least once)
+		endPoints.add(source);
+		while (currentNode != destination) {
+
+			//getting successors
+			Object[] succ = graph.successors(currentNode).toArray();
+			for (Object node : succ) {
+				if(graph.edgeValue((Integer) node, currentNode).isPresent() && !MyAi.searchList(visitedNodes, (Integer)node) && (Integer)node != source) {
+					Integer edgeVal = graph.edgeValue((Integer) node, currentNode).get();
+					Integer distance = (nodeDict.get(currentNode).get(0) + edgeVal);
+
+					//only update the distance if its shorter than our shortest to that node
+					if(distance < nodeDict.get(node).get(0)) { nodeDict.put((Integer) node, new ArrayList<Integer>(Arrays.asList(distance, currentNode))); }
+					//all endpoints can be distinct
+					if(!endPoints.contains((Integer) node)) { endPoints.add((Integer) node); } //add this node to the endpoints list
+
+				}
+			}
+			visitedNodes[pos] = currentNode;
+
+			endPoints.removeIf(Predicate.isEqual(currentNode)); //removes current node (its been fully visited)
+			Integer localBestDist = Integer.MAX_VALUE; Integer d;
+			ArrayList<Integer> debugDistances = new ArrayList<Integer>();
+			for(Integer node : endPoints){// selects the shortest distance of all endpoints bar the ones already visited
+				d = nodeDict.get(node).get(0);
+				debugDistances.add(d);
+				if(d < localBestDist){
+					currentNode = node;
+					localBestDist = d;
+				}
+			}
+			//currentNode = bestNode;
+			pos++;
+		}
+
+
+		List<Integer> path = new ArrayList<Integer>();
+
+		path.add(0, currentNode);
+		while (true) {
+			currentNode = nodeDict.get(currentNode).get(1);
+			path.add(0, currentNode);
+			if(Objects.equals(currentNode, source)) {
+				System.out.println("Path: " +path);
+				return path;
+			} //needs to include source
+		}
+	}
+
+	private List<Move> countMoves(List<Integer> path, Board board) {
+		var x = board.getAvailableMoves()
+				.stream()
+				.filter(z -> z.commencedBy(player))
+				.map(y -> y.destination)
+				.toList();
+		if (x.contains(nextNode));
+		return null;
+	}
+
+	private Integer score() {
+		//score elements:
+		//distance from detectives (tickets away)
+		//available moves
+		//then call minimax
+
+		return null;
+	}
+
+	private Move minimax(Player player) {
+		return null;
+	}
 
 	@Nonnull @Override public Move pickMove(
 			@Nonnull Board board,
