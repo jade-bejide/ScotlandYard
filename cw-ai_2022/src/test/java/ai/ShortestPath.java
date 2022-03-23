cda86415d75748e666bef1b854857a5489941109
elliotmb <elliotbuckingham14@gmail.com>
2022-03-23 12:04:34 +0000

    minimax fleshed out and prepared

--- cw-ai_2022/src/test/java/ai/ShortestPath.java
+++ cw-ai_2022/src/test/java/ai/ShortestPath.java
@@ -1,7 +1,7 @@
 package uk.ac.bris.scotlandyard.ai;
 
 public class ShortestPath {
-    public static main(String[] args) {
+    public static void main(String[] args) {
         Player mrY = new Player(Piece.MrX.MRX, defaultMrXTickets(), 82);
         Player blue = new Player(Piece.Detective.BLUE, defaultDetectiveTickets(), 69);
         Player green = new Player(Piece.Detective.GREEN, defaultDetectiveTickets(), 115);
